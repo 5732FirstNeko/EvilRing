b05188e155b81537b5b7a04f7c11b668185404e9
5732FirstNeko <l43473239@126.com>
2026-05-09 11:32:21 +0800

    Reapply "5/9"
    This reverts commit d3a54ce3b011f8550a901635b5c51ca121724262.

--- Assets/Data/HostitlyUnitData/Witch/SkillData/Witch_core_standrd.cs
+++ Assets/Data/HostitlyUnitData/Witch/SkillData/Witch_core_standrd.cs
@@ -118,7 +118,7 @@ public class Witch_core_standrd : UnitSkillDataSo
                 GetUnitPlatByUnitSite(UnitSite.first).plat;
 
             pot.UnitPlatInit(FactorySystem.instance.EmptyHostitlyUnitData, UnitSite.first);
-            pot.iconSpriteRender.sprite = FactorySystem.instance.EmptyHostitlyUnitData.UnitSprite;
+            pot.iconSpriteRender.sprite = FactorySystem.instance.EmptyHostitlyUnitData.BattleSprite;
             user.costumvalue_second = 0;
         }
 
@@ -194,7 +194,7 @@ public class Witch_core_standrd : UnitSkillDataSo
 
                 spwanUnit.UnitPlatInit(potCard, UnitSite.first);
 
-                spwanUnit.iconSpriteRender.sprite = potCard.UnitSprite;
+                spwanUnit.iconSpriteRender.sprite = potCard.BattleSprite;
                 Vector3 originScale = spwanUnit.transform.localScale;
                 spwanUnit.transform.localScale = Vector3.zero;
                 spwanUnit.transform.DOScale(originScale, 1.5f);
